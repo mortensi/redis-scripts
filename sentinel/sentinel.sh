62d93577685d3ca5dedde6d9f13e97a72d4c2628
Mirko Ortensi <mortensi@mirko.ortensi-TN97MGGHPH>
2024-05-04 00:06:59 +0200

    renamed master

--- sentinel/sentinel.sh
+++ sentinel/sentinel.sh
@@ -8,7 +8,9 @@ case "$1" in
 		sleep 3
                 redis-server --port 2222 --slaveof 127.0.0.1 1111 &
                 sleep 3
-		redis-server ./sentinel.conf --port 5001 --sentinel &
+		redis-server ./sentinel.conf --port 6001 --sentinel &
+                sleep 3
+                redis-server ./sentinel.conf --port 6002 --sentinel &
                 ;;
 	stop)
 		echo "Stopping.."
@@ -16,12 +18,15 @@ case "$1" in
 		sleep 3
                 redis-cli -p 2222 shutdown
 		sleep 3
-                redis-cli -p 5001 shutdown
+                redis-cli -p 6001 shutdown
+		sleep 3
+		redis-cli -p 6002 shutdown
 		;;
 	status)
 		redis-cli -p 1111 ping
                 redis-cli -p 2222 ping
-                redis-cli -p 5001 ping
+                redis-cli -p 6001 ping
+		redis-cli -p 6002 ping
 		;;
 	*)
 		echo "Usage: start, stop, status"
