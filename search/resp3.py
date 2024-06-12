9a319913baa3c41d3195d8c996a5814fc4d7bba2
Mirko Ortensi <mortensi@mirko.ortensi-TN97MGGHPH>
2024-06-12 14:22:22 +0200

    examples refined

--- search/resp3.py
+++ search/resp3.py
@@ -9,23 +9,23 @@ r = redis.Redis(host="localhost", port=6379, protocol=2, decode_responses=True)
 indexes = r.execute_command("FT._LIST")
 
 try:
-    r.ft("doc_idx").dropindex(delete_documents=True)
+    r.ft("document_idx").dropindex(delete_documents=True)
 except:
     pass
 
-index_def = IndexDefinition(prefix=["doc:"], index_type=IndexType.HASH)
+index_def = IndexDefinition(prefix=["document:"], index_type=IndexType.HASH)
 schema = (  TagField("category", as_name="category"),
             TagField("title", as_name="title"),
             NumericField("updated", as_name="updated"),
             TextField("content", as_name="content", weight=1.5))
-r.ft('doc_idx').create_index(schema, definition=index_def)
+r.ft('document_idx').create_index(schema, definition=index_def)
 
-r.hset("doc:1", mapping={"content" : "a book about 8 bits computers", "title":"About computers"})
-r.hset("doc:2", mapping={"content" : "a book about cooking", "title":"About cooking"})
-r.hset("doc:3", mapping={"content" : "A book about electronics", "title":"About electronics"})
+r.hset("document:1", mapping={"content" : "a book about 8 bits computers", "title":"About computers"})
+r.hset("document:2", mapping={"content" : "a book about cooking", "title":"About cooking"})
+r.hset("document:3", mapping={"content" : "A book about electronics", "title":"About electronics"})
 
-res = r.ft("doc_idx").search(Query("cooking"))
-#print(res)
+res = r.ft("document_idx").search(Query("cooking"))
+print(res)
 
 # using JSON
 
@@ -48,28 +48,8 @@ r.json().set("json:2", "$", {"content" : "a book about cooking", "title":"About 
 r.json().set("json:3", "$", {"content" : "A book about electronics", "title":"About electronics"})
 
 res = r.ft("doc_json_idx").search(Query("cooking"))
-#print(res)
-
-# ---------------------------------
-
-r = redis.Redis(host="localhost", port=6379, protocol=2, decode_responses=True)
-
-r.delete("names")
-r.json().set("names", "$", {})
-r.json().set("names", "$.list", [])
-r.json().arrappend("names", "$.list", {"mirko": 47})
-r.json().arrappend("names", "$.list", {"ana": 45})
-res = r.json().get("names")
-
 print(res)
 
-r = redis.Redis(host="localhost", port=6379, protocol=3, decode_responses=True)
-
-res = r.json().get("names")
-#print(res)
 
 
 
-from rejson import Client, Path
-res = ["John", "Doe", "Jane"]
-r.jsonset("mykey", Path.rootPath(), res)
